b038718c6f4da179ca4d936a914c3465bde92910
Paulo Filho <pcf.feat@gmail.com>
2021-03-09 10:17:43 -0400

    feat: add first react component

--- fundamentals/src/App.jsx
+++ fundamentals/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import { RepositoryList } from './components/RepositoryList';
 import './styles/globals.scss';
 
 export function App() {
-  return <h1>Hello World</h1>;
+  return <RepositoryList />;
 }
