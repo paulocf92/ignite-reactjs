03e9dc4d97f33e9aed1391f15bd5a25faa2ce99b
Paulo Filho <pcf.feat@gmail.com>
2021-03-15 16:21:11 -0400

    feat: split modal logic into a new component

--- first-webapp/src/App.tsx
+++ first-webapp/src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
 
@@ -26,12 +27,10 @@ function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
