82f516000afb8454d8bcbee991ea1b0581e4877d
Paulo Filho <pcf.feat@gmail.com>
2021-03-15 16:59:30 -0400

    feat: add modal styles

--- first-webapp/src/components/NewTransactionModal/index.tsx
+++ first-webapp/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
 import { Container } from './styles';
 
 interface NewTransactionModalProps {
@@ -17,6 +18,14 @@ export function NewTransactionModal({
       overlayClassName='react-modal-overlay'
       className='react-modal-content'
     >
+      <button
+        type='button'
+        onClick={onRequestClose}
+        className='react-modal-close'
+      >
+        <img src={closeImg} alt='Fechar modal' />
+      </button>
+
       <Container>
         <h2>Cadastrar transação</h2>
 
