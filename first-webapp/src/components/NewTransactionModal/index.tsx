cf53b65449cd7d56ee03b7bdbe17abec4bf107b9
Paulo Filho <pcf.feat@gmail.com>
2021-03-15 17:33:53 -0400

    feat: finetune selected type button highlight colors

--- first-webapp/src/components/NewTransactionModal/index.tsx
+++ first-webapp/src/components/NewTransactionModal/index.tsx
@@ -43,6 +43,7 @@ export function NewTransactionModal({
             type='button'
             onClick={() => setType('deposit')}
             isActive={type === 'deposit'}
+            activeColor='green'
           >
             <img src={incomeImg} alt='Entrada' />
             <span>Entrada</span>
@@ -52,6 +53,7 @@ export function NewTransactionModal({
             type='button'
             onClick={() => setType('withdraw')}
             isActive={type === 'withdraw'}
+            activeColor='red'
           >
             <img src={outcomeImg} alt='Saída' />
             <span>Saída</span>
