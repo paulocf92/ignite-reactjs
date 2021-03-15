933d3ab019ead0d99aee0b295a210d77ff174781
Paulo Filho <pcf.feat@gmail.com>
2021-03-15 17:14:33 -0400

    feat: add type buttons

--- first-webapp/src/components/NewTransactionModal/index.tsx
+++ first-webapp/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -32,6 +34,18 @@ export function NewTransactionModal({
         <input placeholder='Título' />
 
         <input type='number' placeholder='Valor' />
+
+        <TransactionTypeContainer>
+          <button type='button'>
+            <img src={incomeImg} alt='Entrada' />
+            <span>Entrada</span>
+          </button>
+
+          <button type='button'>
+            <img src={outcomeImg} alt='Saída' />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input placeholder='Categoria' />
 
