c39c009018f26f9c3beb7f8d60e6ce6eb1a3c72b
Paulo Filho <pcf.feat@gmail.com>
2021-03-16 10:44:56 -0400

    feat: calculate balance based on transactions

--- first-webapp/src/components/Summary/index.tsx
+++ first-webapp/src/components/Summary/index.tsx
@@ -9,6 +9,25 @@ import { Container } from './styles';
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
   return (
     <Container>
       <div>
@@ -16,7 +35,12 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt='Entradas' />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.deposits)}
+        </strong>
       </div>
 
       <div>
@@ -24,7 +48,13 @@ export function Summary() {
           <p>Saídas</p>
           <img src={outcomeImg} alt='Saídas' />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>
+          -{' '}
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.withdraws)}
+        </strong>
       </div>
 
       <div className='highlight-background'>
@@ -32,7 +62,12 @@ export function Summary() {
           <p>Total</p>
           <img src={totalImg} alt='Total' />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
