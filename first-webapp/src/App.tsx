51fc260ace3052bf9953a0ba7f35aed8ece81cec
Paulo Filho <pcf.feat@gmail.com>
2021-03-16 10:06:50 -0400

    feat: add transactions loading through hooks

--- first-webapp/src/App.tsx
+++ first-webapp/src/App.tsx
@@ -4,7 +4,7 @@ import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 
 import { GlobalStyle } from './styles/global';
 
@@ -24,7 +24,7 @@ function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -34,7 +34,7 @@ function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
 
---
+++ first-webapp/src/TransactionsContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from './services/api';
+
+export interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: 'deposit' | 'withdraw';
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionsContext = createContext<Transaction[]>([]);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api
+      .get('transactions')
+      .then(response => setTransactions(response.data.transactions));
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={transactions}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
