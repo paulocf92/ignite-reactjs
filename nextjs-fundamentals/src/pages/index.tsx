b153bc0589b75076c65a72e6504e71672edd352e
Paulo Filho <pcf.feat@gmail.com>
2021-03-22 08:31:05 -0400

    feat: add css modules for styles

--- nextjs-fundamentals/src/pages/index.tsx
+++ nextjs-fundamentals/src/pages/index.tsx
@@ -1,5 +1,5 @@
+import styles from '../styles/home.module.css';
+
 export default function Home() {
-  return (
-    <h1>Hello World</h1>
-  )
+  return <h1 className={styles.title}>Hello World</h1>;
 }
