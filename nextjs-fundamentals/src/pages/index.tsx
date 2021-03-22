9df259d001d5d1927282129011bba8fa82160345
Paulo Filho <pcf.feat@gmail.com>
2021-03-22 08:58:51 -0400

    feat: add global styles

--- nextjs-fundamentals/src/pages/index.tsx
+++ nextjs-fundamentals/src/pages/index.tsx
@@ -1,16 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
-        Hello <span>World</span>
-      </h1>
+      <h1>Hello World</h1>
     </>
   );
 }
