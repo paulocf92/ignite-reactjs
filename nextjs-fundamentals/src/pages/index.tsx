82d1333878c2bf54ab4ca7f8d20cd85f3820f30a
Paulo Filho <pcf.feat@gmail.com>
2021-03-22 08:34:37 -0400

    feat: add sass styling

--- nextjs-fundamentals/src/pages/index.tsx
+++ nextjs-fundamentals/src/pages/index.tsx
@@ -1,5 +1,9 @@
-import styles from '../styles/home.module.css';
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>;
+  return (
+    <h1 className={styles.title}>
+      Hello <span>World</span>
+    </h1>
+  );
 }
