1f5f7bcd7da8de97756e42c8e66ba95f5f9ac917
Paulo Filho <pcf.feat@gmail.com>
2021-03-23 10:42:49 -0400

    feat: store user in faunadb

--- nextjs-serverless-jamstack/src/pages/api/auth/[...nextauth].tsx
+++ nextjs-serverless-jamstack/src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,17 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        const { email } = user;
+
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
