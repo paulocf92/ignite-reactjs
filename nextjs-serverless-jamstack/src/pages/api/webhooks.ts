34d40c756411e685bd0103f5f32da3c35f00f7fb
Paulo Filho <pcf.feat@gmail.com>
2021-03-27 09:14:52 -0400

    chore: further listen to more events

--- nextjs-serverless-jamstack/src/pages/api/webhooks.ts
+++ nextjs-serverless-jamstack/src/pages/api/webhooks.ts
@@ -20,7 +20,11 @@ export const config = {
   },
 };
 
-const relevantEvents = new Set(['checkout.session.completed']);
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -44,17 +48,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
-          case 'checkout.session.completed': {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString()
+            );
+
+            break;
+          case 'checkout.session.completed':
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
 
             break;
-          }
           default:
             throw new Error('Unhandled event');
         }
